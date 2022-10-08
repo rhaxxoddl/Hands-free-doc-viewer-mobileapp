d440ca1dd01b2f85a9d52f30b7ffb454b9cf88dd
SangHwanJeon <rhaxxoddl@naver.com>
2022-10-08 17:51:35 +0900

    open_document 1.0.5 패키지 추가

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:document_viewer_m_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const DocWithEye());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
