2b45454785ed4bbcfdc403224dbeb0d72906ed1b
SangHwanJeon <rhaxxoddl@naver.com>
2022-10-08 17:44:37 +0900

    constants.dart 생성

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color cPrimaryColor = Color(0xFF1757EB);
+const Color cBackgroundColor = Color(0xFFFFFFFF);
+const Color cPointColor = Color(0xFF002F9C);
+
+const TextStyle tTitleTextStyle =
+    TextStyle(color: cPrimaryColor, fontWeight: FontWeight.bold);
